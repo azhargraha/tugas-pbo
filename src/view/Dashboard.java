1a57ae38deb2e4dca9c9f57a10e9c4555ad9621c
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2021-12-25 00:23:17 +0700

    dashboard page styling created

--- src/view/Dashboard.java
+++ src/view/Dashboard.java
@@ -12,78 +12,146 @@ public class Dashboard extends javax.swing.JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButton4 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ongoingServiceTable = new javax.swing.JTable();
+        profileButton = new javax.swing.JButton();
+        serviceButton = new javax.swing.JButton();
+        paymentButton = new javax.swing.JButton();
+        historyButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(1280, 720));
-        setPreferredSize(new java.awt.Dimension(1280, 720));
+        setMaximumSize(new java.awt.Dimension(628, 540));
+        setMinimumSize(new java.awt.Dimension(628, 500));
+        setPreferredSize(new java.awt.Dimension(628, 500));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        jPanel2.setMaximumSize(new java.awt.Dimension(1280, 720));
+        jPanel2.setMinimumSize(new java.awt.Dimension(1280, 720));
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(68, 68, 68));
         jLabel1.setText("Ongoing service");
 
-        jButton4.setBackground(new java.awt.Color(255, 255, 255));
-        jButton4.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        jButton4.setForeground(new java.awt.Color(200, 0, 0));
-        jButton4.setText("Logout");
-        jButton4.setBorder(null);
-        jButton4.setBorderPainted(false);
-        jButton4.setFocusPainted(false);
-        jButton4.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-
-        jLabel2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(68, 68, 68));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        jLabel2.setText("Username");
-
         jLabel3.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(68, 68, 68));
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/assets/app-logo.png"))); // NOI18N
+
+        ongoingServiceTable.setBackground(new java.awt.Color(255, 255, 255));
+        ongoingServiceTable.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        ongoingServiceTable.setForeground(new java.awt.Color(255, 255, 255));
+        ongoingServiceTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Tanggal", "ID", "Tipe", "Status", "Harga", "Teknisi"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(ongoingServiceTable);
+        if (ongoingServiceTable.getColumnModel().getColumnCount() > 0) {
+            ongoingServiceTable.getColumnModel().getColumn(0).setResizable(false);
+            ongoingServiceTable.getColumnModel().getColumn(1).setResizable(false);
+            ongoingServiceTable.getColumnModel().getColumn(4).setResizable(false);
+            ongoingServiceTable.getColumnModel().getColumn(5).setResizable(false);
+        }
+
+        profileButton.setBackground(new java.awt.Color(255, 255, 255));
+        profileButton.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        profileButton.setForeground(new java.awt.Color(0, 165, 171));
+        profileButton.setText("Profil");
+        profileButton.setBorder(null);
+        profileButton.setBorderPainted(false);
+        profileButton.setFocusPainted(false);
+
+        serviceButton.setBackground(new java.awt.Color(255, 255, 255));
+        serviceButton.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        serviceButton.setForeground(new java.awt.Color(0, 165, 171));
+        serviceButton.setText("Service");
+        serviceButton.setBorder(null);
+        serviceButton.setBorderPainted(false);
+        serviceButton.setFocusPainted(false);
+
+        paymentButton.setBackground(new java.awt.Color(255, 255, 255));
+        paymentButton.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        paymentButton.setForeground(new java.awt.Color(0, 165, 171));
+        paymentButton.setText("Pembayaran");
+        paymentButton.setBorder(null);
+        paymentButton.setBorderPainted(false);
+        paymentButton.setFocusPainted(false);
+
+        historyButton.setBackground(new java.awt.Color(255, 255, 255));
+        historyButton.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        historyButton.setForeground(new java.awt.Color(0, 165, 171));
+        historyButton.setText("History");
+        historyButton.setBorder(null);
+        historyButton.setBorderPainted(false);
+        historyButton.setFocusPainted(false);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(468, 468, 468)
-                .addComponent(jLabel3)
-                .addGap(97, 97, 97)
+                .addGap(54, 54, 54)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 517, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(0, 304, Short.MAX_VALUE)
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(71, 71, 71)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(65, 65, 65)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGap(71, 71, 71)
+                                .addComponent(jLabel3))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(profileButton, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(serviceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(paymentButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(historyButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(709, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(22, 22, 22)
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton4)))
-                .addGap(68, 68, 68)
+                .addGap(37, 37, 37)
+                .addComponent(jLabel3)
+                .addGap(26, 26, 26)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(profileButton)
+                    .addComponent(serviceButton)
+                    .addComponent(paymentButton)
+                    .addComponent(historyButton))
+                .addGap(42, 42, 42)
                 .addComponent(jLabel1)
-                .addContainerGap(449, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(298, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 720));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 480));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -98,10 +166,14 @@ public class Dashboard extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton historyButton;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable ongoingServiceTable;
+    private javax.swing.JButton paymentButton;
+    private javax.swing.JButton profileButton;
+    private javax.swing.JButton serviceButton;
     // End of variables declaration//GEN-END:variables
 }
