5b6b235494ab6006127b67c6cd58eab502800c93
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2021-12-25 00:23:42 +0700

    dashboard page created

--- src/controller/TubesPBO.java
+++ src/controller/TubesPBO.java
@@ -4,7 +4,7 @@ import view.*;
 
 public class TubesPBO {
     public static void main(String[] args) {
-        Profile profile = new Profile();
-        profile.show();
+        Dashboard test = new Dashboard();
+        test.show();
     }
 }
