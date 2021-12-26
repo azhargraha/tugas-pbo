038f1664e0adf0ed46e62dd18530c3addafda6dd
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2021-12-26 16:19:37 +0700

    .

--- src/controller/TubesPBO.java
+++ src/controller/TubesPBO.java
@@ -4,7 +4,7 @@ import view.*;
 
 public class TubesPBO {
     public static void main(String[] args) {
-        Certificate test = new Certificate();
+        Login test = new Login();
         test.show();
     }
 }
