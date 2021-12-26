7075fa25149f684cbbd2fcbeedd8373570c63f4b
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2021-12-26 20:48:38 +0700

    edit service functionality added

--- src/controller/TubesPBO.java
+++ src/controller/TubesPBO.java
@@ -4,7 +4,7 @@ import view.*;
 
 public class TubesPBO {
     public static void main(String[] args) {
-        editService test = new editService();
+        Login test = new Login();
         test.show();
     }
 }
